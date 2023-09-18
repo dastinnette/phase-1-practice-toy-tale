c8684cb618f1c61e86575367bb966d41b91cd308
David Stinnette <dastinnette@gmail.com>
2023-09-18 11:45:53 -0400

    Added toy info to card

--- src/index.js
+++ src/index.js
@@ -13,3 +13,39 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const url = 'http://localhost:3000/toys'
+const toyCollection = document.getElementById('toy-collection')
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+
+    data.forEach(toyObj => {
+      const div = document.createElement('div')
+      div.classList.add('card')
+      console.log(div)
+
+      const name = document.createElement('h2')
+      div.appendChild(name)
+      name.textContent = toyObj.name
+
+      const img = document.createElement('img')
+      div.appendChild(img)
+      img.classList.add('toy-avatar')
+      img.src = toyObj.image
+
+      const likes = document.createElement('p')
+      div.appendChild(likes)
+      likes.textContent = `${toyObj.likes} likes`
+
+      const button = document.createElement('button')
+      div.appendChild(button)
+      button.classList.add('like-btn')
+      button.textContent = "Like ❤️"
+      button.id = toyObj.id
+
+      toyCollection.append(div)
+    })
+  })
